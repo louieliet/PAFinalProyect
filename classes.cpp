5fe91e8d65594221e84b331f2307ed9a9ebb785b
louieliet <louieliet2003@gmail.com>
2022-04-29 23:50:04 -0500

    29/04

--- classes.cpp
+++ classes.cpp
@@ -3,10 +3,26 @@ using namespace std;
 
 class Student
 {
+private:    
+    char name[20], department[20];
+    int matricula, credits;
+public:
+    Student(int matricula,char name[20],char department[] ,int credits)
+    {
+        this->matricula=matricula;
+        *this->name=*name;
+        *this->department=*department;
+        this->credits=credits;
+    };
 
 };
 
 class Takes
+{
+
+};
+
+class Course
 {
 
 };
