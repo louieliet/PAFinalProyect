0897d58118cd62195bb531c1a736ab499b79e941
louieliet <louieliet2003@gmail.com>
2022-05-03 22:27:26 -0500

    03/05
    Co-authored-by: lizzytrujillosalgado130503@gmail.com <lizzytrujillosalgado130503@gmail.com>

--- source.h
+++ source.h
@@ -6,7 +6,6 @@
 #include <cstring>
 using namespace std;
 class Student{
-    
 private:    
     char fname[15], lname[10], department[7];
     int matricula, credits;
@@ -29,7 +28,8 @@ public:
         cout << "Student registered succesfuly!";
     }
 
-    void setFirstName(const string& FirstName){
+    void setFirstName(const string& FirstName)
+    {
         size_t length{FirstName.size()};
         length = (length < 15 ? length : 14);
         FirstName.copy(fname, length);
@@ -51,7 +51,7 @@ public:
     }
     void setInts(int tuition, int credits)
     {
-        matricula=tuition;
+        this->matricula=tuition;
         this->credits=credits;
     }
 
