8819d0c7e0682993247e89d0c56360499c866323
louieliet <louieliet2003@gmail.com>
2022-05-04 13:08:31 -0500

    04/05

--- ProyectHeader.h
+++ ProyectHeader.h
@@ -16,48 +16,33 @@ public:
     ~Student(){};
     void registerStudent()
     {
-    string fn,ln;
+        string fn,ln;
 
-    cout << "\t- Student  -" << endl;
-    cout << "Nombre, Apellido" << endl;
-    cin >> fn >> ln;
-    setFirstName(fn);
-    setLastName(ln);
-    system("cls");
-    cout << "Student registered succesfuly!";
+        cout << "\t- Student  -" << endl;
+        cout << "Nombre, Apellido" << endl;
+        cin >> fn >> ln;
+        setFirstName(fn);
+        setLastName(ln);
+        system("cls");
+        cout << "Student registered succesfuly!";
     }
     void setFirstName(const string & FirstName) 
     {
         size_t length {FirstName.size()};
-        length = (
-            length < 15
-                ? length
-                : 14
-            );
+        length = (length < 15 ? length : 14);
         FirstName.copy(fname, length);
         fname[length] = '\0';
     }
     void setLastName(const string & LastName) {
         size_t length {LastName.size()};
-        length = (
-            length < 10
-                ? length
-                : 9
-        );
+        length = (length < 10 ? length : 9);
         LastName.copy(lname, length);
         lname[length] = '\0';
     }
-
     void outName(ofstream& file)
     {
-        for(int i=0; i<15;i++)
-        {
-            file << fname[i];
-        }
-        for(int i=0; i<10;i++)
-        {
-            file << lname[i];
-        }
+        for(int i=0; i<15;i++) {file << fname[i];}
+        for(int i=0; i<10;i++) {file << lname[i];}
         
     }
 };
